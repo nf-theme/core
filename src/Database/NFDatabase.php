6e6d372cbd790790c7df6e8f9afa1bc01a6f238c
Hieupv <hieupv@codersvn.com>
2017-12-07 15:14:12 +0700

    fix class binding issue for NFDatabase

--- src/Database/NFDatabase.php
+++ src/Database/NFDatabase.php
@@ -2,12 +2,14 @@
 
 namespace NFWP\Database;
 
+use NF\Database\DBManager;
+use NF\Facades\App;
+
 class NFDatabase
 {
     public function __construct($plugin_file = __FILE__)
     {
-        $manager = DBManager::getInstance();
-        $manager->bootEloquent();
+        App::make(DBManager::class)->bootEloquent();
 
         if (method_exists($this, 'up')) {
             register_activation_hook($plugin_file, [$this, 'up']);
